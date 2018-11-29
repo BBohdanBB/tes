71871b80dd51fa0e1a7412f1e946eba66126e86e
Bogdan <bogdanfirst0@gmail.com>
2018-11-30 01:58:28 +0200

    change app name

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
-MyApp = Flask(__name__)
+icc = Flask(__name__)
 
 @MyApp.route("/")
 def hello():
 	return render_template('index.html')
 
 if __name__ == "__main__":
-	MyApp.run()
+	icc.run()
